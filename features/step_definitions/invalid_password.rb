665dff949e6f05b9d45a9774446fabaa722437f7
Jing-wei Wu <topwu.tw@gmail.com>
2016-08-29 23:38:57 +0800

    Incorrect string compare when check error messages

--- features/step_definitions/invalid_password.rb
+++ features/step_definitions/invalid_password.rb
@@ -1,5 +1,9 @@
 Then /^I should see error (.*?)$/ do |error|
 	wait_for_elements_exist("* id:'alertTitle'", :timeout => 1)
-	query("* id:'message'", "text").first == error
+
+	message = query("* id:'message'", "text").first
+	unless message != error
+		fail "wrong error message: '#{message}'"
+	end
 end
 
