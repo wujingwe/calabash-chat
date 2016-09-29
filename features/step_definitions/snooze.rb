8a4110a44fb4632ab16a59e020b81ec5bbcd0fb0
Fredaisgood <freda.synology@gmail.com>
2016-09-29 21:31:01 +0800

    modify snooze feature

--- features/step_definitions/snooze.rb
+++ features/step_definitions/snooze.rb
@@ -4,25 +4,21 @@ Given (/^I am in snooze view$/) do
 	if element_does_not_exist("* id:'drawer'")
 		touch("Toolbar AppCompatImageButton")
 	end
+	sleep 2
 	touch("* id:'snooze'")
 end
 
 Then (/^I set the snooze time for "(.*?)"$/) do |input|
-     touch("* id:'#{input}'")
+    touch("* id:'#{input}'")
 end
 
-Then (/^I check snooze time is correct or not/) do 
+Then (/^I check snooze time is correct or not/) do
+	sleep 5 
     touch("* id:'snooze'")
 	remain1 = query("* id:'remain'").last['text']
-	sleep5
+	sleep 5
 	remain2 = query("* id:'remain'").last['text'] 
-	if(remain1 > remain2)
-		return true
-	else
+	if (remain1 <= remain2)
 		raise "incorrect snooze time"
 	end
 end
-	
-	
-
-		
